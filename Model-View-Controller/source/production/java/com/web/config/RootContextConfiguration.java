fd7ecb0e4c8596a9fdbf5c110c07fd953c9844f9
Egan <1154610080@qq.com>
2018-09-01 10:27:50 +0800

    配置消息转换器

--- Model-View-Controller/source/production/java/com/web/config/RootContextConfiguration.java
+++ Model-View-Controller/source/production/java/com/web/config/RootContextConfiguration.java
@@ -1,7 +1,12 @@
 package com.web.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 
 @Configuration
@@ -10,4 +15,24 @@ import org.springframework.stereotype.Controller;
         excludeFilters = @ComponentScan.Filter(Controller.class)
 )
 public class RootContextConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        //查找并注册所有扩展模块
+        mapper.findAndRegisterModules();
+        //禁止日期序列化为时间戳整数
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        //禁止将反序列化得到的日期调整为当前时区
+        mapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
+        return mapper;
+    }
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller(){
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan(new String[]{"com.web.site"});
+        return marshaller;
+    }
+
 }
--- Model-View-Controller/source/production/java/com/web/config/ServletContextConfiguration.java
+++ Model-View-Controller/source/production/java/com/web/config/ServletContextConfiguration.java
@@ -1,15 +1,31 @@
 package com.web.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.RequestToViewNameTranslator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.DefaultRequestToViewNameTranslator;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.inject.Inject;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -18,7 +34,45 @@ import org.springframework.web.servlet.view.JstlView;
         useDefaultFilters = false,
         includeFilters = @ComponentScan.Filter(Controller.class)
 )
-public class ServletContextConfiguration {
+public class ServletContextConfiguration extends WebMvcConfigurerAdapter{
+
+    @Inject
+    ObjectMapper objectMapper;
+    @Inject
+    Marshaller marshaller;
+    @Inject
+    Unmarshaller unmarshaller;
+
+
+    /**
+     * 配置消息转换器
+     * 将请求正文转换成控制器方法可处理的java对象
+     **/
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new ByteArrayHttpMessageConverter());
+        converters.add(new StringHttpMessageConverter());
+        converters.add(new FormHttpMessageConverter());
+        converters.add(new SourceHttpMessageConverter<>());
+
+        MarshallingHttpMessageConverter xmlConverter = new MarshallingHttpMessageConverter();
+        xmlConverter.setSupportedMediaTypes(Arrays.asList(
+                new MediaType("application", "xml"),
+                new MediaType("text", "xml")
+        ));
+        xmlConverter.setMarshaller(this.marshaller);
+        xmlConverter.setUnmarshaller(this.unmarshaller);
+        converters.add(xmlConverter);
+
+        MappingJackson2HttpMessageConverter jsonConverter =
+                new MappingJackson2HttpMessageConverter();
+        jsonConverter.setSupportedMediaTypes(Arrays.asList(
+                new MediaType("application", "json"),
+                new MediaType("text", "json")
+        ));
+        jsonConverter.setObjectMapper(this.objectMapper);
+        converters.add(jsonConverter);
+    }
 
     /**
      * 配置视图名称转换

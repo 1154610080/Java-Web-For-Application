8c6c796ee99f077e2f1f3ef016d1e33c2caa6671
Egan <1154610080@qq.com>
2018-09-01 08:36:21 +0800

    配置视图名称转换

--- Model-View-Controller/source/production/java/com/web/config/ServletContextConfiguration.java
+++ Model-View-Controller/source/production/java/com/web/config/ServletContextConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.RequestToViewNameTranslator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.DefaultRequestToViewNameTranslator;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -18,6 +20,22 @@ import org.springframework.web.servlet.view.JstlView;
 )
 public class ServletContextConfiguration {
 
+    /**
+     * 配置视图名称转换
+     * 去除Web应用上下文URL和URL结尾的文件扩展名，
+     * 剩下的URL变成视图名称
+     * 如 http://localhost:8080/mvc/foo/bar.html将转换成视图名称 foo/bar
+     **/
+    @Bean
+    public RequestToViewNameTranslator viewNameTranslator()
+    {
+        return new DefaultRequestToViewNameTranslator();
+    }
+
+    /**
+     * 配置视图解析
+     * 将视图名称转换成JSP文件名
+     **/
     @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver resolver =

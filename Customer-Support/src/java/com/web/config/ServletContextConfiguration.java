9fdc3fd9ad78c29b5610bd9efd6ae7ee6c90d5c4
Egan <1154610080@qq.com>
2018-09-02 14:35:06 +0800

    添加内容协商、视图解析、Multipart支持配置

--- Customer-Support/src/java/com/web/config/ServletContextConfiguration.java
+++ Customer-Support/src/java/com/web/config/ServletContextConfiguration.java
@@ -1,6 +1,7 @@
 package com.web.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -14,8 +15,16 @@ import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.RequestToViewNameTranslator;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.DefaultRequestToViewNameTranslator;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 import javax.inject.Inject;
 import java.util.Arrays;
@@ -63,5 +72,42 @@ public class ServletContextConfiguration extends WebMvcConfigurerAdapter{
         ));
         jsonConverter.setObjectMapper(this.objectMapper);
         converters.add(jsonConverter);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(true).favorParameter(false)
+                .parameterName("mediaType").ignoreAcceptHeader(false)
+                .useJaf(false).defaultContentType(MediaType.APPLICATION_XML)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("json", MediaType.APPLICATION_JSON);
+    }
+    
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        InternalResourceViewResolver resolver = 
+                new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+    
+    @Bean
+    public RequestToViewNameTranslator viewNameTranslator(){
+        return new DefaultRequestToViewNameTranslator();
+    }
+    
+    /**
+     * 启用Multipart支持
+     *
+     * @date 2018/9/2 14:33  
+     * @param    
+     * @return org.springframework.web.multipart.MultipartResolver 
+     **/  
+    @Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
     }
 }

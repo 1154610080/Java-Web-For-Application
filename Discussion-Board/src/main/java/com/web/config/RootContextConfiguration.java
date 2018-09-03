40f9118a3aaaf037cb189617c94d82cbb1e57de5
Egan <1154610080@qq.com>
2018-09-03 22:14:43 +0800

    创建讨论版项目并完成基本配置

---
+++ Discussion-Board/src/main/java/com/web/config/RootContextConfiguration.java
@@ -0,0 +1,30 @@
+package com.web.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+@ComponentScan(basePackages = "com.web.site")
+public class RootContextConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
+        return mapper;
+    }
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller(){
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan(new String[]{"com.web.site"});
+        return marshaller;
+    }
+}

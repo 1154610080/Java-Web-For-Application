73b9937379ca960721dea5c7fffc0e8af8f3b8a0
Egan <1154610080@qq.com>
2018-09-21 19:32:16 +0800

    使用principal替代过滤器的session

--- src/java/com/web/site/AuthenticationFilter.java
+++ src/java/com/web/site/AuthenticationFilter.java
@@ -2,9 +2,11 @@ package com.web.site;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.security.Principal;
 
 /**
  * 验证过滤器
@@ -24,13 +26,22 @@ public class AuthenticationFilter implements Filter{
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpSession session = ((HttpServletRequest)request).getSession(false);
 
-        if(session == null || session.getAttribute("username") == null)
+        HttpSession session = ((HttpServletRequest)request).getSession(false);
+        Principal principal = UserPrincipal.getPrincipal(session);
+
+        if(principal == null)
             ((HttpServletResponse)response).sendRedirect(
                     ((HttpServletRequest)request).getContextPath() + "/login");
-        else
-            chain.doFilter(request, response);
+        else{
+
+            chain.doFilter(new HttpServletRequestWrapper((HttpServletRequest) request){
+                @Override
+                public Principal getUserPrincipal() {
+                    return principal;
+                }
+            }, response);
+        }
 
     }
 
--- src/java/com/web/site/LoggingFilter.java
+++ src/java/com/web/site/LoggingFilter.java
@@ -3,10 +3,10 @@ package com.web.site;
 import org.apache.logging.log4j.ThreadContext;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.UUID;
 
 /**
@@ -31,13 +31,16 @@ public class LoggingFilter implements Filter
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        ThreadContext.put("id", UUID.randomUUID().toString());
-        HttpSession session = ((HttpServletRequest)request).getSession(false);
-        if(session != null)
+        String id = UUID.randomUUID().toString();
+        ThreadContext.put("id", id);
+        Principal principal = UserPrincipal.getPrincipal(
+                ((HttpServletRequest)request).getSession(false));
+        if(principal != null)
             ThreadContext.put("username",
-                    (String)session.getAttribute("username"));
+                    principal.getName());
 
         try{
+            ((HttpServletResponse)response).setHeader("X-Wrox-Request-Id", id);
             chain.doFilter(request, response);
         }finally {
             ThreadContext.clearAll();

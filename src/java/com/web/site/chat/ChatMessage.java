eb644f4192d70e32c7ae2ee06ad8b39a1e48aaf8
Egan <1154610080@qq.com>
2018-09-25 21:56:18 +0800

    为ChatMessage组成一个消息代码和参数，以编程形式实现国际化

--- src/java/com/web/site/chat/ChatMessage.java
+++ src/java/com/web/site/chat/ChatMessage.java
@@ -1,18 +1,24 @@
 package com.web.site.chat;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.time.Instant;
 
 /**
  * 聊天消息POJO
+ * 支持国际化
  *
  * @author Egan
  * @date 2018/9/2 15:45
  **/
-public class ChatMessage {
+public class ChatMessage implements Cloneable {
     private Instant timeStamp;
     private Type type;
     private String user;
-    private String content;
+    private String contentCode;
+    private Object[] contentArguments;
+    private String localizedContent;
+    private String userContent;
 
     public Instant getTimeStamp() {
         return timeStamp;
@@ -38,16 +44,65 @@ public class ChatMessage {
         this.user = user;
     }
 
-    public String getContent() {
-        return content;
+    public String getContentCode() {
+        return contentCode;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setContentCode(String contentCode) {
+        this.contentCode = contentCode;
+    }
+
+    public Object[] getContentArguments() {
+        return contentArguments;
+    }
+
+    public void setContentArguments(Object[] contentArguments) {
+        this.contentArguments = contentArguments;
+    }
+
+    public String getLocalizedContent() {
+        return localizedContent;
+    }
+
+    public void setLocalizedContent(String localizedContent) {
+        this.localizedContent = localizedContent;
+    }
+
+    public String getUserContent() {
+        return userContent;
+    }
+
+    public void setUserContent(String userContent) {
+        this.userContent = userContent;
     }
 
     public static enum Type{
         STARTED, JOINED, ERROR, LEFT, TEXT
     }
+
+    /**
+     * 以下两个方法是支持Jackson Data Processor 的Mix-in注解特性的特殊类，
+     **/
+    static abstract class MixInForLogWrite{
+        /**
+         * localizedContent不应被写入日志中，因为它是为某个特定用户进行本地化的。
+         **/
+        @JsonIgnore public abstract String getLocalizedContent();
+        @JsonIgnore public abstract void setLocalizedContent();
+    }
+
+
+
+    static abstract class MixInForWebSocket{
+        /**
+         * contentCode和contentArguments不需要Websocket进行传输，
+         * 因为消息已经本地化了
+         **/
+        @JsonIgnore public abstract String getContentCode();
+        @JsonIgnore public abstract void setContentCode(String c);
+        @JsonIgnore public abstract Object[] getContentArguments();
+        @JsonIgnore public abstract void setContentArguments(Object[] c);
+    }
+
 
 }

fca8d9e518bc6f816dfa67956c0a1b552a85aed2
Egan <1154610080@qq.com>
2018-10-01 22:03:11 +0800

    添加验证国际化文件并在根应用上下文中配置

--- src/java/com/web/config/RootContextConfiguration.java
+++ src/java/com/web/config/RootContextConfiguration.java
@@ -108,7 +108,7 @@ public class RootContextConfiguration implements AsyncConfigurer, SchedulingConf
         messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
         messageSource.setBasenames(
                 "/WEB-INF/i18n/titles", "/WEB-INF/i18n/messages",
-                "/WEB-INF/i18n/errors"
+                "/WEB-INF/i18n/errors", "/WEB-INF/i18n/validation"
         );
         return messageSource;
     }
